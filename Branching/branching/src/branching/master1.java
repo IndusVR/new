638799d75d58c0e96551b8e361075a990d06a9cc
IndusVR <iransing@LIN51009621.corp.capgemini.com>
2021-09-20 11:49:34 +0530

    this is first change

--- Branching/branching/src/branching/master1.java
+++ Branching/branching/src/branching/master1.java
@@ -5,6 +5,10 @@ public class master1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is master");
+		show();
+	}
+	public static void show() {
+		System.out.println("This is first change");
 	}
 
 }
